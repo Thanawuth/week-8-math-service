c715c3a522f305daf478ed8bb10273fcec923255
Aftertnw <64160049@go.buu.ac.th>
2024-09-09 21:44:03 +0700

    done math-service

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,17 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3001);
 
   const microApp = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: 8877,
+      },
     },
   );
   await microApp.listen();
